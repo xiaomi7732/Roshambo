8edf4ca68675505288f6e6ee4297e757fc1b141b
Saar Shen <xiaomi7732@hotmail.com>
2022-12-19 10:23:58 -0800

    Append CORS for roshambo

--- Roshambo.Backend/Program.cs
+++ Roshambo.Backend/Program.cs
@@ -12,7 +12,7 @@ builder.Services.Configure<JsonOptions>(options =>
 
 builder.Services.AddCors(options => options.AddDefaultPolicy(builder =>{
     builder
-        .WithOrigins("http://127.0.0.1:5500")
+        .WithOrigins("http://127.0.0.1:5500", "https://roshambo.codewithsaar.com")
         .WithHeaders("content-type")
         .WithMethods("GET", "POST", "OPTIONS");
 }));
